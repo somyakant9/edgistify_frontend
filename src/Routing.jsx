39be72a951b3d0856bd5d3feb9de2aebe4cb4048
somyakant9 <msomya143@gmail.com>
2025-01-21 17:33:36 +0530

    added protected Route

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/login";
 import Products from "./pages/products";
 import Cart from "./pages/cart";
 import Checkout from "./pages/checkout";
+import Protected from "./components/protectedRoute";
 
 export default function Routing() {
   return (
@@ -12,8 +13,8 @@ export default function Routing() {
       <Route path="/" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/cart" element={<Protected><Cart /></Protected>} />
+      <Route path="/checkout" element={<Protected><Checkout /></Protected>} />
       {/* Define routes for Login, Products, and Cart later */}
     </Routes>
   );
